8fc52f417896cbcbf2010a195a6facee45280d8f
Arjun Gandhi <account@arjungandhi.com>
2021-05-19 12:08:22 -0400

    made it not templatey i think

--- C610Bus.h
+++ C610Bus.h
@@ -6,12 +6,12 @@
 
 enum class C610Subbus { kIDZeroToThree, kIDFourToSeven };
 
-template <CAN_DEV_TABLE _bus = CAN1>
 class C610Bus {
  public:
   static const uint8_t kSize = 8;
 
  private:
+  CAN_DEV_TABLE bus;
   static const uint32_t kIDZeroToThreeCommandID = 0x200;
   static const uint32_t kFourToSevenCommandID = 0x1FF;
   static const uint32_t kReceiveBaseID = 0x200;
@@ -19,14 +19,14 @@ class C610Bus {
 
   bool is_initialized_ = false;
   C610 controllers_[kSize];
-  FlexCAN_T4<_bus, RX_SIZE_256, TX_SIZE_16> can_;
+  FlexCAN_T4<bus, RX_SIZE_256, TX_SIZE_16> can_;
 
   void InitializeCAN();
 
   static void TorqueToBytes(int16_t torque, uint8_t &upper, uint8_t &lower);
 
  public:
-  C610Bus();
+  C610Bus(CAN_DEV_TABLE bus);
   void PollCAN();
   void Callback(const CAN_message_t &msg);
   void CommandTorques(const int32_t torque0, const int32_t torque1 = 0,
@@ -34,5 +34,3 @@ class C610Bus {
                       C610Subbus subbus = 0);
   C610 &Get(const uint8_t i);
 };
-
-#include "C610Bus.tpp"
